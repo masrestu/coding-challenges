362cbe59c6d9d6bd78a12355696103e58f4b6da9
masrestu <n.rizkiawan@gmail.com>
2025-06-13 18:07:16 +0700

    chore(build): regenerate production build assets

--- frontend-mentor/testimonials-grid-section/src/components/TestimonialCard.jsx
+++ frontend-mentor/testimonials-grid-section/src/components/TestimonialCard.jsx
@@ -1,12 +1,10 @@
 import "./TestimonialCard.css"
 
 export default function TestimonialCard({ data }) {
-    const imageUrl = new URL(data.avatar, import.meta.url).href;
-
     return (
         <div className={`testimonial-card ${data.colorTheme}`}>
             <div className="testimonial-header">
-                <img src={imageUrl} alt="" className="person-image" />
+                <img src={data.avatar} alt="" className="person-image" />
                 <div className="person-info">
                     <h3 className="person-name">{data.name}</h3>
                     <p className="person-title">{data.title}</p>
