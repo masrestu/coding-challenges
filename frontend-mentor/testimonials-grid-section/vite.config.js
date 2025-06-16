7084a31e3759c0ff2d171c9ca7aa31f11a4b8d78
masrestu <n.rizkiawan@gmail.com>
2025-06-16 10:48:21 +0700

    update

--- frontend-mentor/testimonials-grid-section/vite.config.js
+++ frontend-mentor/testimonials-grid-section/vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
-
-  build: {
-    outDir: '.', // This is the default output directory
-  },
 })
