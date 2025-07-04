1296039d43751b00338285b9d50506d0d87b4e6d
masrestu <n.rizkiawan@gmail.com>
2025-07-04 15:39:34 +0700

    chores(Frontend Quiz App): update build config

--- frontend-mentor/frontend-quiz-app/react-app/vite.config.js
+++ frontend-mentor/frontend-quiz-app/react-app/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         tailwindcss()
     ],
     build: {
-        outDir: path.resolve('../dist'),
+        outDir: path.resolve('../preview'),
         emptyOutDir: true,
     },
     base: './'
